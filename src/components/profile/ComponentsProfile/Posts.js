7dad43d7840ef5d038605e4518f2a09b6d99ec6c
Nyrmehametov danil <danilka468@outlook.com>
2021-03-01 19:09:28 +0300

    16 урок

--- src/components/profile/ComponentsProfile/Posts.js
+++ src/components/profile/ComponentsProfile/Posts.js
@@ -10,8 +10,8 @@ const Posts = () => {
                 <button>Add post</button>
                 <button>Remove</button>
             </div>
-            <Post/>
-
+            <Post photourl="https://i.pinimg.com/originals/bf/75/9b/bf759b9beeecf5be32f514aa94b23705.jpg" message="Pisun"/>
+            <Post photourl="https://steamuserimages-a.akamaihd.net/ugc/784123877859907552/4A76FF0BA2ABA14FA73D5D291E493C8D11535D9E/?imw=512&amp;imh=409&amp;ima=fit&amp;impolicy=Letterbox&amp;imcolor=%23000000&amp;letterbox=true" message="davar"/>
         </div>
     );
 };
