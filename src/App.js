2c8192fc2017250fb65b89db61e9088396b0de2b
DVAR <danilka070103@gmail.com>
2021-03-01 22:25:52 +0300

    18

--- src/App.js
+++ src/App.js
@@ -3,14 +3,19 @@ import './App.css';
 import Header from "./components/header/Header";
 import Navbar from "./components/navbar/Navbar";
 import Profile from "./components/profile/Profile";
-import  "./style.css";
+import "./style.css";
+import Dialogs from "./components/dialogs/Dialogs";
 
 const App = () => {
     return (
         <div className='app-wrapper'>
             <Header/>
             <Navbar/>
-            <Profile/>
+
+            <div className='app-wrapper-content'>
+                <Profile/>
+                {/*<Dialogs/>*/}
+            </div>
         </div>
     );
 }
---
+++ src/components/dialogs/Dialogs.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Classes from './dialog.module.css'
+
+const Dialogs = (props) => {
+    return (
+        <div className={Classes.content}>
+            dialog
+        </div>
+    );
+};
+
+export default Dialogs;
