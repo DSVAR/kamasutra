1f8540bc9b4846c6c66266667541295d90c702ad
DVAR <danilka070103@gmail.com>
2021-03-17 20:37:05 +0300

    38

--- src/Redux/State.js
+++ src/Redux/State.js
@@ -1,57 +1,67 @@
+let store={
+    _State: {
 
-let rerender=()=>{
-    console.log("debian");
-}
-let State = {
+        dialogs: {
+            users: [
+                {
+                    ID: '0',
+                    Name: 'DDvar',
+                    Url: 'https://avatars.mds.yandex.net/get-zen_doc/985972/pub_5d63ef4cf73d9d00adeba678_5d63f06bf73d9d00adeba67e/scale_1200'
+                },
+                {ID: '1', Name: "RAtataat", Url: 'https://i.ytimg.com/vi/iVFmoi6N9B0/maxresdefault.jpg'}
+            ],
+            messages: [
+                {ID: 0, Nick: "Me", Message: "Hello",Right:true},
+                {ID: 1, Nick: "DDvar", Message: "Hi,How are u?",Right: false}
+            ],
+            newMessage:'ItDava'
 
-    dialogs: {
-        users: [
-            {
-                ID: '0',
-                Name: 'DDvar',
-                Url: 'https://avatars.mds.yandex.net/get-zen_doc/985972/pub_5d63ef4cf73d9d00adeba678_5d63f06bf73d9d00adeba67e/scale_1200'
-            },
-            {ID: '1', Name: "RAtataat", Url: 'https://i.ytimg.com/vi/iVFmoi6N9B0/maxresdefault.jpg'}
-        ],
-        messages: [
-            {ID: 0, Nick: "Me", Message: "Hello",Right:true},
-            {ID: 1, Nick: "DDvar", Message: "Hi,How are u?",Right: false}
-        ],
-        newMessage:'ItDava'
+        },
+        sidebar:{
+            friends:[
+                {Id:'0',Name:'Davar',Url:'https://w7.pngwing.com/pngs/78/95/png-transparent-pentagram-pentacle-satanism-wicca-symbol-symbol-miscellaneous-monochrome-magic.png'},
+                {Id:'1',Name:'Ratat',Url:'https://w7.pngwing.com/pngs/78/95/png-transparent-pentagram-pentacle-satanism-wicca-symbol-symbol-miscellaneous-monochrome-magic.png'},
+                {Id:'2',Name:'Debik',Url:'https://w7.pngwing.com/pngs/78/95/png-transparent-pentagram-pentacle-satanism-wicca-symbol-symbol-miscellaneous-monochrome-magic.png'}
+            ]
+        }
 
     },
-    sidebar:{
-        friends:[
-            {Id:'0',Name:'Davar',Url:'https://w7.pngwing.com/pngs/78/95/png-transparent-pentagram-pentacle-satanism-wicca-symbol-symbol-miscellaneous-monochrome-magic.png'},
-            {Id:'1',Name:'Ratat',Url:'https://w7.pngwing.com/pngs/78/95/png-transparent-pentagram-pentacle-satanism-wicca-symbol-symbol-miscellaneous-monochrome-magic.png'},
-            {Id:'2',Name:'Debik',Url:'https://w7.pngwing.com/pngs/78/95/png-transparent-pentagram-pentacle-satanism-wicca-symbol-symbol-miscellaneous-monochrome-magic.png'}
-            ]
+    getState(){
+        return this._State;
+    },
+    rerender()
+    {
+        console.log("debian");
+    },
+    AddMessage()
+    {
+
+        let MessageNew={
+            ID:2,
+            Message:this._State.dialogs.newMessage,
+            Nick:"Me",
+            Right:true
+        };
+
+        this._State.dialogs.messages.push(MessageNew);
+        this._State.dialogs.newMessage='';
+        this.rerender(this._State)
+    },
+    updateMessage(text)
+    {
+
+        this._State.dialogs.newMessage=text;
+        this.rerender(this._State)
+    },
+    subscribe(observer){
+        this.rerender=observer;
+
     }
-
 }
 
-export const AddMessage=()=>{
 
-    let MessageNew={
-        ID:2,
-        Message:State.dialogs.newMessage,
-        Nick:"Me",
-        Right:true
-    };
 
-    State.dialogs.messages.push(MessageNew);
-    State.dialogs.newMessage='';
-    rerender(State)
-}
 
-export const updateMessage=(text)=>{
 
-    State.dialogs.newMessage=text;
-    rerender(State)
-}
 
-export const subscribe=(observer)=>{
-    rerender=observer;
-
-}
-export default State;
+export default store;
