a24a222031803e0343d229a8a381ec601a657520
DSVAR <danilka070103@gmail.com>
2021-04-22 08:47:35 +0300

    49

--- src/components/profile/ComponentsProfile/PostsContainer.js
+++ src/components/profile/ComponentsProfile/PostsContainer.js
@@ -7,7 +7,7 @@ let mapStateToProps=(state)=>{
     return{
         posts:state.posts.post,
         textNew:state.posts.textNew
-    }
+    }//Отдаем props +отслеживаем состояние компонента
 }
 let mapDispatchToProps=(dispatch)=>{
     return{
@@ -16,7 +16,7 @@ let mapDispatchToProps=(dispatch)=>{
         
         addPost:()=>{dispatch(addPostActionCreator())}
         
-    }
+    }//отдаем callbacks
 }
 const  PostsContainer=connect(mapStateToProps,mapDispatchToProps)(Posts)
 
