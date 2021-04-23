d1538e8197e796a79dac67b788b5a7f29dbd8c58
DVAR <danilka070103@gmail.com>
2021-04-23 07:09:20 +0300

    дописать юзер ксс модуль

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import Classes from './User.Module.css';
 
 let Users=(props)=>{
+    debugger;
     return(
-        <div>
-           {props.users.map(u => <div key={u.Id}>{u.Name} </div>)}
+        
+        <div className={Classes.main_View}>
+           {props.users.map(u =>
+               <div key={u.Id} className={Classes.item}>
+                   <img src={u.Img}/>
+                   
+               <p >{u.Name} </p>
+               <p >{u.Locatinon.city} </p>
+               </div>
+           )}
         </div>
     )
 }
