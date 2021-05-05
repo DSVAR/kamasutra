2ad6d4413720ac1d1790f634cc0251789fea2e43
DVAR <danilka070103@gmail.com>
2021-05-05 06:10:12 +0300

    59

--- src/components/common/Preloader/Preloader.js
+++ src/components/common/Preloader/Preloader.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import preLoaderGif from './../../../Images/loading.gif';
+import Classes from './Style.Module.css';
 
 let Preloader=(props)=>{
     
      return (
         
-     <div
-        >{props.isFetching ? <img src={preLoaderGif}/> : null}
+     <div >
+         {props.isFetching ? <img className={Classes.sizeLoader} src={preLoaderGif}/> : null}
      </div>)
 }
 export default Preloader;
