860839db557ffb7f027f2af8cb1a8c5c74ba04cf
DVAR <danilka070103@gmail.com>
2021-05-31 22:10:14 +0300

    91

---
+++ src/components/Users/User.js
@@ -0,0 +1,44 @@
+﻿import React from "react";
+import Classes from './User.Module.css';
+import {NavLink} from "react-router-dom";
+import Paginator from "./Paginator";
+
+
+let User = ({user, isFollowingProcensing, follow, unfollow}) => {
+    let photoUrl = 'https://avatars.mds.yandex.net/get-zen_doc/985972/pub_5d63ef4cf73d9d00adeba678_5d63f06bf73d9d00adeba67e/scale_1200';
+
+
+    return (
+        <div className={Classes.item}>
+            <div>
+                <NavLink to={'/profile/' + user.id}>
+                    <img className={Classes.photoUser}
+                         src={user.photos.small != null ? user.photos.small : photoUrl}/>
+                </NavLink>
+            </div>
+            <div>
+
+                {user.followed ?
+                    <button disabled={isFollowingProcensing.some(id => id === user.id)} onClick={() => {
+                        unfollow(user.id)
+
+                    }}> unfollow</button>
+
+
+                    :
+                    <button disabled={isFollowingProcensing.some(id => id === user.id)} onClick={() => {
+                        follow(user.id)
+                    }
+                    }> follow</button>}
+            </div>
+            <div>
+                <p>{user.name} </p>
+            </div>
+            <div>
+                <p>{user.status != null ? user.status : "i'm batman"} </p>
+            </div>
+        </div>
+    )
+}
+
+export default User;
